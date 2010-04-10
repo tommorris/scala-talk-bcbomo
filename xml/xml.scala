3055d72e07388d0e8d6d9215971309935fac9d0b
Tom Morris <tom@tommorris.org>
2010-04-10 08:41:53 +0100

    finished off inline XML syntax example.

--- xml/xml.scala
+++ xml/xml.scala
@@ -14,3 +14,22 @@ val xmldoc = XML.loadString(xmlString)
 assert(xmldoc.isInstanceOf[scala.xml.Elem])
 
 /* Now, let us load in a bigger document. As a literal. */
+
+/* Demo */
+
+/* Now how about writing some XML? */
+
+class Person {
+  var firstName: Option[String] = None
+  var lastName: Option[String] = None
+  
+  def toXml() = <Person>
+	{ if (this.firstName.isDefined) <firstName>{this.firstName.get}</firstName> }
+	{ if (this.lastName.isDefined) <lastName>{this.lastName.get}</lastName> }
+  </Person>
+}
+
+var tom = new Person
+tom.firstName = Some("Tom")
+tom.lastName = Some("Morris")
+println(tom.toXml.toString)
